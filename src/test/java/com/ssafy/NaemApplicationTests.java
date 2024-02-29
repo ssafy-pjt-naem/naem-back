4bc0ee753546a8a1b4b84cce44e7f8b5a1152771
chwangmin <ckm7907cb@gmail.com>
2024-02-29 15:15:18 +0900

    :tada: init project

---
+++ src/test/java/com/ssafy/NaemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
