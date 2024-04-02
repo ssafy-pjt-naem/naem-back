0998c99781e64a59ae0603ef918eae98a9c6cae5
최광민 <ckm7907@naver.com>
2024-04-02 13:24:45 +0900

    feat: 엔티티 기본 맵핑

--- src/main/java/com/ssafy/domain/board/model/Board.java
+++ src/main/java/com/ssafy/domain/board/model/Board.java
@@ -1,14 +1,22 @@
 package com.ssafy.domain.board.model;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
 public class Board {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String name;
+    @Column(nullable = false, length = 50)
     private Date created;
+    @Column(length = 50)
     private Date updated;
+    @Column(nullable = false)
     private Status status;
 
 }
--- src/main/java/com/ssafy/domain/member/model/Member.java
+++ src/main/java/com/ssafy/domain/member/model/Member.java
@@ -2,15 +2,23 @@ package com.ssafy.domain.member.model;
 
 import com.fasterxml.jackson.annotation.JsonTypeId;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 50)
     private String nickname;
+    @Column(nullable = false, length = 50)
     private Date created;
+    @Column(length = 50)
     private Date updated;
+    @Column(nullable = false)
     private Status status;
 
 }
