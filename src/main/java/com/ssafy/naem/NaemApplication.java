80dd63373063f0868e90dd56a88e7ae0c487eb74
chwangmin <ckm7907cb@gmail.com>
2024-05-30 17:07:00 +0900

    init

---
+++ src/main/java/com/ssafy/naem/NaemApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.naem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaemApplication.class, args);
+	}
+
+}
