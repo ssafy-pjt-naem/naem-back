7cd7d380e4ebf542b35b918f15dfd8c599ecacfc
chwangmin <ckm7907cb@gmail.com>
2024-06-19 14:15:33 +0900

    feat: Cors (localhost:5173) 추가

---
+++ src/main/java/com/ssafy/naem/global/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.ssafy.naem.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
