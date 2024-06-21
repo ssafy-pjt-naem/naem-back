27cb4697398e3dcd7784399a190276253479b39c
변재호 <bjho606@naver.com>
2024-06-22 02:10:18 +0900

    chore, docs: swagger api 추가
    - build.gradle 에 swagger 관련 추가
    - swagger config 추가
    - 게시판 api 명세 추가

--- src/main/java/com/ssafy/naem/domain/board/dto/request/BoardChangeNameRequest.java
+++ src/main/java/com/ssafy/naem/domain/board/dto/request/BoardChangeNameRequest.java
@@ -1,4 +1,7 @@
 package com.ssafy.naem.domain.board.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "게시판 이름은 중복될 수 없음")
 public record BoardChangeNameRequest(String name){
 }
